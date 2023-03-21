d5247f69b993874c7042b51f44be205b231e954e
robin <robinmahmud358@gmail.com>
2023-03-21 12:49:19 +0600

    first commit

---
+++ src/Components/Layout/Main.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import { Outlet } from 'react-router-dom';
+import Header from '../Share/Header/Header';
+import Navbar from '../Share/Nabvar/Navbar';
+
+const Main = () => {
+    return (
+        <div>
+            <Header></Header>
+            <Outlet></Outlet>
+            <Navbar></Navbar>
+        </div>
+    );
+};
+
+export default Main;
