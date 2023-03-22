cfaf478e5fc409c3f2de10eb00c7de7098bc6195
robin <robinmahmud358@gmail.com>
2023-03-22 23:36:33 +0600

    same implementation

--- src/Components/Page/Home/About.js
+++ src/Components/Page/Home/About.js
@@ -2,7 +2,7 @@ import React from 'react';
 
 const About = () => {
     return (
-        <div className='mt-[50px]'>
+        <div className='mt-[100px]'>
             <h1 className='text-[#ffc000] text-3xl mb-3 lg:mr-3 lg:ml-3'>About TYT</h1>
             <p className='text-sm text-[#5f6368] lg:mr-3 lg:ml-3'>The Yellow Therapist is an integrated platform that aspires to uplift the existing state of mental health services of Bangladesh. Removing as many obstacles as possible between the one who needs help and the one who can provide- is what we are striving for. In our platform, the therapists and the ones who need the therapy can easily get in touch with each other. At the end of the day, it's the connection that will bring positive outcomes. We are relentlessly working for a better tomorrow where no soul suffers, where every heart is at peace.</p>
         </div>
--- src/Components/Page/Home/Payment.js
+++ src/Components/Page/Home/Payment.js
@@ -2,7 +2,7 @@ import React from 'react';
 
 const Payment = () => {
     return (
-        <div className='mt-10'>
+        <div className='mt-[100px]'>
             <h1 className='text-[#5f6368] font-bold text-center  mb-3'>Secure and easy payment with wide range of checkout choices</h1>
             <img src="https://images.theyellowtherapist.com/logos/FOOTER-02.png" alt="" />
             <hr />
